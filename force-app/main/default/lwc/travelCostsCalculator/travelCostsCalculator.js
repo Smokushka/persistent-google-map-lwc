0172b2ca6411fe2905d97acc47a289e3419ad302
Alex Shabanov <alex.ashabanov@atco.com>
2023-06-11 15:03:28 -0400

    Wire getPredictionsFor method

--- force-app/main/default/lwc/travelCostsCalculator/travelCostsCalculator.js
+++ force-app/main/default/lwc/travelCostsCalculator/travelCostsCalculator.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
+import getPredictionsFor from '@salesforce/apex/AddressPickerController.getPredictionsFor';
 
-export default class TravelCostsCalculator extends LightningElement {}
+export default class TravelCostsCalculator extends LightningElement {
+    predictions;
+
+    searchKey;
+
+    handleChange(event) {
+        this.searchKey = event.detail.value;
+        console.log( 'searchKey is', this.searchKey );
+
+        getPredictionsFor( { addressInput : this.searchKey } ).then(result => {
+            console.log( 'result is', JSON.stringify( result ) );
+
+            let tempPredictions = [];
+                result.forEach( ( record ) => {
+
+                    let tempRec = Object.assign( {}, record );      
+                    tempRec.description = tempRec.description.replace( new RegExp( this.searchKey, 'i' ),( value ) => `<b>${value}</b>` );                    
+                    tempPredictions.push( tempRec );
+                    
+                });
+                this.predictions = tempPredictions;
+        });
+    }
+
+    handleSelect(event) {
+        let strIndex = event.currentTarget.dataset.id;
+        console.log( 'strIndex is', strIndex );
+    }
+}
